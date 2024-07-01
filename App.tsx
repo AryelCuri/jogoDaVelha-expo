ffcdc79b8f7ffb79d22090aa45150f9f2ce68085
AryelCuri <aryelcuri2@gmail.com>
2024-07-01 09:10:52 -0300

    fix

--- App.tsx
+++ App.tsx
@@ -65,6 +65,31 @@ export default function App() {
     status = "Próximo jogador: " + (vezDoX ? "X" : "O");
   }
 
+    // Função para calcular o vencedor do jogo
+  const calcularVencedor = (quadrados: Array<string | null>): string | null  => {
+      const linhas = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      for (let i = 0; i < linhas.length; i++) {
+        const [a, b, c] = linhas[i];
+        if (
+          quadrados[a] &&
+          quadrados[a] === quadrados[b] &&
+          quadrados[a] === quadrados[c]
+        ) {
+          return quadrados[a];
+        }
+      }
+      return null;
+    }
+
   // Renderizar o componente
   return (
     <View style={styles.container}>
@@ -111,28 +136,3 @@ const styles = StyleSheet.create({
     fontSize: 40,
   },
 });
-
-  // Função para calcular o vencedor do jogo
-  function calcularVencedor(quadrados: Array<string | null>): string | null {
-    const linhas = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < linhas.length; i++) {
-      const [a, b, c] = linhas[i];
-      if (
-        quadrados[a] &&
-        quadrados[a] === quadrados[b] &&
-        quadrados[a] === quadrados[c]
-      ) {
-        return quadrados[a];
-      }
-    }
-    return null;
-  }
